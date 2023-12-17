37926c8c55608d8fef78a4cf5b58cabf5950d7b1
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2023-12-17 19:02:42 +0500

    configure jest file

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
+// Import the next/jest module to create a Jest configuration tailored for Next.js projects.
 const nextJest = require("next/jest");
 
+// Use nextJest to generate the Jest configuration for your Next.js app.
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
+// Define additional custom Jest configuration options.
 const customJestConfig = {
+  // Specify setup files to be executed after Jest's environment is set up, including custom setups.
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  
+  // Set the test environment to jest-environment-jsdom for browser-like testing environment.
   testEnvironment: "jest-environment-jsdom",
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// Combine the generated Next.js Jest configuration with the custom configuration.
+// This ensures that the setup provided by next/jest is augmented with your customizations.
 module.exports = createJestConfig(customJestConfig);
